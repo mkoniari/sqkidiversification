be0353be5cb5312420457d16d9738f7603d77bc8
sadegh.kharazmi <sadegh.kharazmi@gmail.com>
2013-03-25 13:50:44 +0000

    changes made

--- create.qsub.run.for.query.pl
+++ create.qsub.run.for.query.pl
@@ -4,5 +4,5 @@ print "#PBS -l nodes=1:ppn=2,walltime=10:00:00\n";
 #print "#PBS -M sadegh.kharazmi@gmail.com\n";
 print "#PBS -m abe\n";
 print "cd \$PBS_WORKDIR\n";
-print "time java -Xms100M -Xmx4000M -Djava.library.path=/scratch/sadegh/Toolkits/indri-5.3/swig/obj/java/ -classpath /scratch/sadegh/source/sqkidiversification:/scratch/sadegh/source/sqkidiversification/src:/scratch/sadegh/Toolkits/indri-5.3/swig/obj/java/ com.sqki.net.Main $ARGV[0] /scratch/sadegh/source/sqkidiversification/baseline/tesTclueweb 0.5 > /scratch/sadegh/source/sqkidiversification/temp-result/$ARGV[0] \n";
+print "time java -Xms100M -Xmx4000M -Djava.library.path=/scratch/sadegh/Toolkits/indri-5.3/swig/obj/java/ -classpath /scratch/sadegh/source/sqkidiversification:/scratch/sadegh/source/sqkidiversification/src:/scratch/sadegh/Toolkits/indri-5.3/swig/obj/java/ com.sqki.net.Main $ARGV[0] /scratch/sadegh/source/sqkidiversification/baseline/tesTclueweb $ARGV[1] $ARGV[2] > /scratch/sadegh/source/sqkidiversification/temp-result/$ARGV[0]-mmr-$ARGV[1] \n";
 
--- src/com/sqki/net/Main.java
+++ src/com/sqki/net/Main.java
@@ -20,11 +20,16 @@ public class Main {
 	//MAC Index
 	//static String myIndex="/Users/Research/research/Data/FT.Diversity.Index";
 	//HPC index
-	static String myIndex="/scratch/sadegh/DATA/TRECDATA/CluewebCatBIndex-StopWordRemoval-WithDoc";
+
+		//static String myIndex="/scratch/sadegh/DATA/TRECDATA/CluewebCatBIndex-StopWordRemoval-WithDoc";
+		
+		//Lighter HPC index about 167 G
+		static String myIndex="/scratch/sadegh/DATA/TRECDATA/CluewebCatBIndex";
 	//static String myIndex;
 	//on windows
 	//static String myIndex="J:\\Sadegh-Personal\\research\\data\\FT.Diversity.Index";
-	static int cuttoff=100;
+//	static int cuttoff=100;
+	static int cuttoff;
 	public static int getCuttoff() {
 		return cuttoff;
 	}
@@ -34,14 +39,17 @@ public class Main {
 	
 
 	static String resultFile;
-	static String divMethod="scorediff";
 	
-	  public static void main(String[] args) throws Exception {
+//	static String divMethod="scorediff";
+	static String divMethod="mmr";
+	  
+		public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
         // Parameter : topic result lambda
 		  topicNumber = Integer.parseInt(args[0]);
 		  resultFile=args[1];
 		  lambda=Double.parseDouble(args[2]);
+		  cuttoff=Integer.parseInt(args[3]);
 		//  myIndex=args[3];
 		  
 //		  resultFile="baseline/result.okapi";
