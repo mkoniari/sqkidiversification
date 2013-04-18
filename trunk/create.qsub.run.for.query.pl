93905c4db8ea88de439d68c3f95773777a32c93d
sadegh.kharazmi@gmail.com <sadegh.kharazmi@gmail.com@b8c80a13-4d01-ec32-a5ff-374f4ec0f82f>
2013-04-18 16:48:29 +0000

    qsub saved

--- trunk/create.qsub.run.for.query.pl
+++ trunk/create.qsub.run.for.query.pl
@@ -20,7 +20,7 @@ print "#PBS -l nodes=1:ppn=2,walltime=10:00:00\n";
 #print "#PBS -M sadegh.kharazmi@gmail.com\n";
 print "#PBS -m abe\n";
 print "cd \$PBS_WORKDIR\n";
-print "time java -Xms100M -Xmx4000M -Djava.library.path=/scratch/sadegh/Toolkits/indri-5.3/swig/obj/java/ -classpath /scratch/sadegh/source/sqkidiversification:/scratch/sadegh/source/sqkidiversification/src:/scratch/sadegh/Toolkits/indri-5.3/swig/obj/java/ com.sqki.net.Main $ARGV[0] /scratch/sadegh/source/sqkidiversification/baseline/tesTclueweb $ARGV[1] $ARGV[2] \"$query\" $ARGV[3] > /scratch/sadegh/source/sqkidiversification/temp-result/$ARGV[0]-mmr-$ARGV[1] \n";
+print "time java -Xms100M -Xmx4000M -Djava.library.path=/scratch/sadegh/Toolkits/indri-5.3/swig/obj/java/ -classpath /scratch/sadegh/source/sqkidiversification:/scratch/sadegh/source/sqkidiversification/src:/scratch/sadegh/Toolkits/indri-5.3/swig/obj/java/ com.sqki.net.Main $ARGV[0] /scratch/sadegh/source/sqkidiversification/baseline/tesTclueweb $ARGV[1] $ARGV[2] \"$query\" $ARGV[3] > /scratch/sadegh/source/sqkidiversification/temp-result/$ARGV[0]-mmr-$ARGV[1]-W$ARGV[3] \n";
 
 
 sub trim($)
